78880a5ae06ed4afe0a0588405dfdb5a9c09ee28
MIS-SE-Zahid Hasan <Zahid.mis@aci-bd.com>
2023-09-05 16:19:21 +0600

    upto sales

--- app/Http/Controllers/Sales/SalesController.php
+++ app/Http/Controllers/Sales/SalesController.php
@@ -22,37 +22,40 @@ use Illuminate\Support\Facades\Validator;
 class SalesController extends Controller
 {
     use CodeGeneration;
-    public function getSupportingData(){
+
+    public function getSupportingData()
+    {
         $customers = Customer::all();
-        $finalCustomer =[];
-        $data=[];
-        foreach ($customers as $single_data){
-            $data['CustomerWithCode'] = $single_data->CustomerCode.'-'.$single_data->CustomerName;
+        $finalCustomer = [];
+        $data = [];
+        foreach ($customers as $single_data) {
+            $data['CustomerWithCode'] = $single_data->CustomerCode . '-' . $single_data->CustomerName;
             $data['CustomerCode'] = $single_data->CustomerCode;
-            array_push($finalCustomer,$data);
+            array_push($finalCustomer, $data);
         }
-        $allStock = StockBatch::select('ItemCode',DB::raw("SUM(BatchQty) as stock"))->groupBy('ItemCode')->get();
+        $totalStock = StockBatch::select('ItemCode','LocationCode', DB::raw("SUM(BatchQty) as stock"))->groupBy('ItemCode','LocationCode')->get();
 
         return response()->json([
             'status' => 'success',
             'category' => ItemsCategory::all(),
             'customer' => $finalCustomer,
-            'allStock' => $allStock,
+            'allStock' => $totalStock,
             'locations' => Location::all(),
         ]);
     }
-    public function index(Request $request){
+
+    public function index(Request $request)
+    {
         $take = $request->take;
         $search = $request->search;
-        $salesMaster =  SalesMaster::join('SalesDetails', 'SalesDetails.SalesCode', 'SalesMaster.SalesCode')
-            ->join('ItemsCategory','ItemsCategory.CategoryCode','SalesMaster.CategoryCode')
-            ->join('Customer','Customer.CustomerCode','SalesMaster.CustomerCode')
+        $salesMaster = SalesMaster::join('SalesDetails', 'SalesDetails.SalesCode', 'SalesMaster.SalesCode')
+            ->join('ItemsCategory', 'ItemsCategory.CategoryCode', 'SalesMaster.CategoryCode')
+            ->join('Customer', 'Customer.CustomerCode', 'SalesMaster.CustomerCode')
             ->where(function ($q) use ($search) {
                 $q->where('SalesMaster.SalesCode', 'like', '%' . $search . '%');
                 $q->orWhere('SalesMaster.SalesDate', 'like', '%' . $search . '%');
             })
-
-            ->orderBy('SalesMaster.SalesDate', 'desc')
+            ->orderBy('SalesMaster.PrepareDate', 'desc')
             ->select(
                 'SalesMaster.SalesCode',
                 DB::raw("convert(varchar(10),SalesMaster.SalesDate,23) as SalesDate"),
@@ -78,18 +81,20 @@ class SalesController extends Controller
     }
 
     //Category Wise Item
-    public function getCategoryWiseItemData(Request $request){
+    public function getCategoryWiseItemData(Request $request)
+    {
         return response()->json([
             'status' => 'success',
-            'items' => Items::where('CategoryCode',$request->CategoryCode)->get(),
+            'items' => Items::where('CategoryCode', $request->CategoryCode)->get(),
             'locations' => Location::all(),
         ]);
     }
 
     //Check Stock
-    public function getCategoryWiseItemStock(Request $request){
+    public function getCategoryWiseItemStock(Request $request)
+    {
 
-        $totalStock =  StockBatch::select(DB::raw("SUM(BatchQty) as stock"))->where('ItemCode',$request->ItemCode)->groupBy('ItemCode')->first();
+        $totalStock = StockBatch::select('ItemCode', DB::raw("SUM(BatchQty) as stock"))->where('ItemCode', $request->ItemCode)->groupBy('ItemCode')->first();
         $totalStock = floatval($totalStock->stock);
         return response()->json([
             'status' => 'success',
@@ -98,8 +103,8 @@ class SalesController extends Controller
     }
 
     //Store Sales
-    //Store Purchase
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'sales_date' => 'required',
             'categoryType' => 'required',
@@ -108,48 +113,46 @@ class SalesController extends Controller
         ]);
         if ($validator->fails()) {
             return response()->json(['message' => $validator->errors()], 400);
-        }
-        else{
+        } else {
             try {
                 DB::beginTransaction();
 
                 //Data Insert PurchaseMaster
-                $salesCode =  $this->generateSalesMasterCode();
+                $salesCode = $this->generateSalesMasterCode();
 
                 $dataSales = new SalesMaster();
                 $dataSales->SalesCode = $salesCode;
                 $dataSales->SalesDate = $request->sales_date;
                 $dataSales->Reference = $request->reference;
-                $dataSales->CategoryCode =$request->categoryType['CategoryCode'];
-                $dataSales->CustomerCode =$request->customerTypeVal['CustomerCode'];
+                $dataSales->CategoryCode = $request->categoryType['CategoryCode'];
+                $dataSales->CustomerCode = $request->customerTypeVal['CustomerCode'];
                 $dataSales->Returned = 'N';
                 $dataSales->PrepareDate = Carbon::now()->format('Y-m-d H:i:s');;
                 $dataSales->PrepareBy = Auth::user()->Id;
                 $dataSales->save();
 
-                foreach ($request->details as $key=>$singleData){
+                foreach ($request->details as $key => $singleData) {
 
                     //Data Insert ProductionDetails
                     $dataSalesDetails = new SalesDetails();
                     $dataSalesDetails->SalesCode = $salesCode;
-                    $dataSalesDetails->ItemCode = $singleData['item']['ItemCode'];
-                    $dataSalesDetails->LocationCode = $singleData['location']['LocationCode'];
+                    $dataSalesDetails->ItemCode = $singleData['itemCode'];
+                    $dataSalesDetails->LocationCode = $singleData['LocationCode'];
                     $dataSalesDetails->unitPrice = $singleData['unitPrice'];
                     $dataSalesDetails->Quantity = $singleData['quantity'];
                     $dataSalesDetails->Value = $singleData['itemValue'];
                     $dataSalesDetails->save();
 
                     //Data insert into Stock Batch
-                    $existingStockTable = StockBatch::where('ItemCode', $singleData['item']['ItemCode'])->where('LocationCode',$singleData['location']['LocationCode'])->orderBy('ItemCode','desc')->get();
-                    foreach ($existingStockTable as $checkStock){
-                        $existingBatchQty = $checkStock->BatchQty;
-                        $existingStockValue = $checkStock->StockValue;
-                        if($checkStock->BatchQty > $singleData['quantity']){
-                            StockBatch::where('ItemCode', $singleData['item']['ItemCode'])->where('LocationCode',$singleData['location']['LocationCode'])->update([
-                                'BatchQty'=>$existingBatchQty - $singleData['quantity'],
-                                'StockValue'=>$existingStockValue - $singleData['itemValue'],
-                            ]);
-                        }
+                    $checkExisting = StockBatch::where('ItemCode', $singleData['itemCode'])->where('LocationCode', $singleData['LocationCode'])->orderBy('ItemCode', 'desc')->first();
+                    if ($checkExisting) {
+                        $existingBatchQty = $checkExisting->BatchQty;
+                        $existingStockValue = $checkExisting->StockValue;
+                        StockBatch::where('ItemCode', $singleData['itemCode'])->where('LocationCode', $singleData['LocationCode'])->update([
+                            'BatchQty' => $existingBatchQty - $singleData['quantity'],
+                            'StockValue' => $existingBatchQty - $singleData['quantity'],
+                        ]);
+
                     }
                 }
                 DB::commit();
@@ -167,15 +170,21 @@ class SalesController extends Controller
         }
     }
 
-    public function getSalesInfo($salesCode){
-
-        $singleSales =  SalesMaster::join('SalesDetails', 'SalesDetails.SalesCode', 'SalesMaster.SalesCode')
-            ->join('ItemsCategory','ItemsCategory.CategoryCode','SalesMaster.CategoryCode')
-            ->join('Items',function ($q) {
-                $q->on('Items.ItemCode','SalesDetails.ItemCode');
+    public function getSalesInfo($salesCode)
+    {
+        $singleSales = SalesMaster::join('SalesDetails', 'SalesDetails.SalesCode', 'SalesMaster.SalesCode')
+            ->join('ItemsCategory', 'ItemsCategory.CategoryCode', 'SalesMaster.CategoryCode')
+            ->join('Items', function ($q) {
+                $q->on('Items.ItemCode', 'SalesDetails.ItemCode');
             })
-            ->join('Customer','Customer.CustomerCode','SalesMaster.CustomerCode')
-            ->where('SalesMaster.SalesCode',$salesCode)
+            ->join('Location', function ($q) {
+                $q->on('Location.LocationCode', 'SalesDetails.LocationCode');
+            })
+            ->join('Customer', 'Customer.CustomerCode', 'SalesMaster.CustomerCode')
+            ->leftJoin('StockBatch', function ($q) {
+                $q->on('StockBatch.ItemCode', 'SalesDetails.ItemCode')->on('StockBatch.LocationCode', 'SalesDetails.LocationCode');
+            })
+            ->where('SalesMaster.SalesCode', $salesCode)
             ->select(
                 'SalesMaster.SalesCode',
                 DB::raw("convert(varchar(10),SalesMaster.SalesDate,23) as SalesDate"),
@@ -187,16 +196,33 @@ class SalesController extends Controller
                 DB::raw("(CASE WHEN Customer.CustomerCode IS NOT NULL THEN (Customer.CustomerCode +'-'+Customer.CustomerName) END) AS CustomerWithCode"),
                 'SalesMaster.Returned',
                 DB::raw("convert(varchar(10),SalesMaster.PrepareDate,23) as PrepareDate"),
-
-
                 'SalesDetails.ItemCode',
                 'SalesDetails.LocationCode',
                 'SalesDetails.UnitPrice',
                 'SalesDetails.Quantity',
                 'SalesDetails.Value',
-                'Items.ItemName'
-
+                DB::raw("SUM(StockBatch.BatchQTY) as stock"),
+                'Items.ItemName',
+                'Items.UOM',
+                'Location.LocationName'
             )
+            ->groupBy('SalesMaster.SalesCode',
+                'SalesMaster.SalesDate',
+                'SalesMaster.Reference',
+                'SalesMaster.CategoryCode',
+                'ItemsCategory.CategoryName',
+                'Customer.CustomerName',
+                'Customer.CustomerCode',
+                'SalesMaster.Returned',
+                'SalesMaster.PrepareDate',
+                'SalesDetails.ItemCode',
+                'SalesDetails.LocationCode',
+                'SalesDetails.UnitPrice',
+                'SalesDetails.Quantity',
+                'SalesDetails.Value',
+                'Items.ItemName',
+                'Items.UOM',
+                'Location.LocationName')
             ->get();
 
         return response()->json([
@@ -206,8 +232,8 @@ class SalesController extends Controller
     }
 
     //Update
-    public function update(Request $request){
-        dd($request);
+    public function update(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'sales_date' => 'required',
             'categoryType' => 'required',
@@ -216,49 +242,68 @@ class SalesController extends Controller
         ]);
         if ($validator->fails()) {
             return response()->json(['message' => $validator->errors()], 400);
-        }
-        else{
+        } else {
             try {
                 DB::beginTransaction();
 
-                $dataSales = SalesMaster::where('SalesCode',$request->salesCode)->first();
+                $dataSales = SalesMaster::where('SalesCode', $request->salesCode)->first();
                 $dataSales->SalesDate = $request->sales_date;
                 $dataSales->Reference = $request->reference;
-                $dataSales->CustomerCode =$request->customerTypeVal['CustomerCode'];
+                $dataSales->CustomerCode = $request->customerTypeVal['CustomerCode'];
                 $dataSales->Returned = 'N';
                 $dataSales->EditDate = Carbon::now()->format('Y-m-d H:i:s');;
                 $dataSales->EditBy = Auth::user()->Id;
                 $dataSales->save();
 
-                //Data Insert ProductionDetails
-                $dataSalesDetails = SalesDetails::where('SalesCode',$request->salesCode)->get();
-
-                foreach ($dataSalesDetails as $singleDetail){
-                    //Data insert into Stock Batch
-                    $existingStockTable = StockBatch::where('ItemCode', $singleDetail['ItemCode'])->where('LocationCode',$singleDetail['LocationCode'])->first();
-                    $existingBatchQty = $existingStockTable->BatchQty;
-                    $existingStockValue = $existingStockTable->StockValue;
-
-                    StockBatch::where('ItemCode', $singleDetail['ItemCode'])->where('LocationCode',$singleDetail['LocationCode'])->update([
-                        'BatchQty'=>$existingBatchQty + $singleDetail['quantity'],
-                        'StockValue'=>$existingStockValue + $singleDetail['Value'],
-                    ]);
-                   // dd('dd');
-
-                }
 
                 //delete and insert the existing
-                foreach ($request->details as $key=>$singleData){
-                    $existingSalesDetails =  SalesDetails::where('SalesCode',$request->salesCode)
-                        ->where('ItemCode', $singleData['item']['ItemCode'])
-                        ->where('LocationCode',$singleData['location']['LocationCode'])->delete();
+                foreach ($request->details as $key => $singleData) {
+
+                    $existingSalesDetail = SalesDetails::where('SalesCode', $request->salesCode)
+                        ->where('ItemCode', $singleData['itemCode'])
+                        ->where('LocationCode', $singleData['LocationCode'])->first();
+
+                    //Data update  into Stock Batch
+                    $existingStockTable = StockBatch::where('ItemCode',$singleData['itemCode'])
+                        ->where('LocationCode',$singleData['LocationCode'])
+                        ->first();
+
+                    if($existingStockTable){
+                        $existingBatchQty = $existingStockTable->BatchQty;
+                        $existingStockValue = $existingStockTable->StockValue;
+
+                        if($existingSalesDetail['Quantity']>$singleData['quantity']) {
+                            $UpdateLessQuantity = $existingSalesDetail['Quantity'] - $singleData['quantity'];
+                            //Back Existing Product
+                            StockBatch::where('ItemCode', $singleData['itemCode'])->where('LocationCode', $singleData['LocationCode'])->update([
+                                'BatchQty' => floatval($existingBatchQty) + floatval($UpdateLessQuantity),
+                                'StockValue' => floatval($existingBatchQty) + floatval($UpdateLessQuantity),
+                            ]);
+                        }
+                        else{
+
+                            $UpdateGreaterQuantity = $singleData['quantity'] - $existingSalesDetail['Quantity']  ;
+                            StockBatch::where('ItemCode', $singleData['itemCode'])->where('LocationCode',$singleData['LocationCode'])->update([
+                                'BatchQty'=>floatval($existingBatchQty) -  floatval($UpdateGreaterQuantity),
+                                'StockValue'=>floatval($existingBatchQty) -  floatval($UpdateGreaterQuantity),
+                            ]);
+
+                        }
+                    }
+                    if($existingSalesDetail){
+                        $existingPurchaseDetails = SalesDetails::where('SalesCode', $request->salesCode)
+                            ->where('ItemCode', $singleData['itemCode'])
+                            ->where('LocationCode', $singleData['LocationCode'])
+                            ->delete();
+                    }
+
 
 
                     //Data Insert ProductionDetails
                     $dataSalesDetails = new SalesDetails();
                     $dataSalesDetails->SalesCode = $dataSales->SalesCode;
-                    $dataSalesDetails->ItemCode = $singleData['item']['ItemCode'];
-                    $dataSalesDetails->LocationCode = $singleData['location']['LocationCode'];
+                    $dataSalesDetails->ItemCode = $singleData['itemCode'];
+                    $dataSalesDetails->LocationCode = $singleData['LocationCode'];
                     $dataSalesDetails->unitPrice = $singleData['unitPrice'];
                     $dataSalesDetails->Quantity = $singleData['quantity'];
                     $dataSalesDetails->Value = $singleData['itemValue'];
@@ -268,6 +313,49 @@ class SalesController extends Controller
                 return [
                     'status' => 'success',
                     'message' => 'Sales Updated Successfully'
+                ];
+
+            } catch (\Exception $exception) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => $exception->getMessage()
+                ], 500);
+            }
+        }
+    }
+
+    //Return
+    public function returnProducts(Request $request){
+        if($request->salesCode){
+            try {
+                DB::beginTransaction();
+
+                //Data Insert SalesMaster
+                $dataPurchase = SalesMaster::where('SalesCode',$request->salesCode)->first();
+                $dataPurchase->Returned = 'Y';
+                $dataPurchase->EditDate = Carbon::now()->format('Y-m-d H:i:s');;
+                $dataPurchase->EditBy = Auth::user()->Id;
+                $dataPurchase->save();
+
+                $existingSalesDetails =  SalesDetails::where('SalesCode',$request->salesCode)->get();
+
+                foreach ($existingSalesDetails as $key=>$singleData){
+                    //Data update  into Stock Batch
+                    $existingStockTable = StockBatch::where('ItemCode', $singleData->ItemCode)->where('LocationCode',$singleData['LocationCode'])->first();
+                    if($existingStockTable){
+                        $existingBatchQty = $existingStockTable->BatchQty;
+                        $existingStockValue = $existingStockTable->StockValue;
+
+                        StockBatch::where('ItemCode', $singleData->ItemCode)->where('LocationCode',$singleData['LocationCode'])->update([
+                            'BatchQty'=>floatval($existingBatchQty) + floatval($singleData->Quantity),
+                            'StockValue'=>floatval($existingStockValue) + floatval($singleData->Quantity),
+                        ]);
+                    }
+                }
+                DB::commit();
+                return [
+                    'status' => 'success',
+                    'message' => 'Sales Returned Successfully'
                 ];
 
             } catch (\Exception $exception) {
--- resources/js/components/purchase/AddEditModal.vue
+++ resources/js/components/purchase/AddEditModal.vue
@@ -164,7 +164,7 @@
                                                     </select>
                                                 </td>
                                                 <td>
-                                                    <input type="text"  class="form-control"  @input="setValue(index)"
+                                                    <input type="number"  class="form-control"  @input="setValue(index)" style="text-align: end"
                                                            v-model="field.unitPrice" placeholder="unit Price" min="1">
                                                     <span class="error"
                                                           v-if="errors[index] !== undefined && errors[index].unitPrice !== undefined">{{
@@ -172,7 +172,7 @@
                                                         }}</span>
                                                 </td>
                                                 <td>
-                                                    <input type="text"  class="form-control"
+                                                    <input type="number"  class="form-control" style="text-align: end"
                                                            v-model="field.quantity" placeholder="quantity"  @input="setValue(index)" min="1">
                                                     <span class="error"
                                                           v-if="errors[index] !== undefined && errors[index].quantity !== undefined">{{
@@ -181,8 +181,8 @@
 
                                                 </td>
                                                 <td>
-                                                    <input type="text" class="form-control" readonly
-                                                           v-model="field.itemValue" placeholder="Value" min="1">
+                                                    <input type="number" class="form-control" readonly style="text-align: end"
+                                                           v-model="field.itemValue" placeholder="Value">
                                                     <span class="error"
                                                           v-if="errors[index] !== undefined && errors[index].itemValue !== undefined">{{
                                                             errors[index].itemValue
