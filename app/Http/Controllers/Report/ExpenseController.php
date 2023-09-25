26cb214988893b142107779aa50742bc8315cdf0
rahamanopu <rahamanopu@gmail.com>
2023-09-25 09:51:23 +0600

    new two report implement

---
+++ app/Http/Controllers/Report/ExpenseController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Report;
+
+use App\Http\Controllers\Controller;
+use App\Models\StockBatch;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class ExpenseController extends Controller
+{
+    public function index(Request $request){
+        if(!empty($request->filters[0]['value'])){
+            $first = $request->filters[0]['value'][0];
+            $second = $request->filters[0]['value'][1];
+
+            $dateFrom = date("Y-m-d", strtotime($first));
+            $dateTo = date("Y-m-d", strtotime($second));
+        }
+        else{
+            $dateFrom = Carbon::now()->format('Y-m-d');;
+            $dateTo =  Carbon::now()->format('Y-m-d');;
+        }
+        if(!empty($request->filters[1]['value'])){
+            $catergoryCode  = $request->filters[1]['value'];
+        }
+        else{
+            $catergoryCode = '%';
+        }
+
+        $result =  DB::select("exec sp_Expenses '$dateFrom','$dateTo','$catergoryCode' ");
+
+        return response()->json([
+            'data' => $result
+        ]);
+    }
+}
