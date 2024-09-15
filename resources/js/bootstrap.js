4d4c63f1fcb7b74375cc86ea17e1affb7912dd0b
shojib_shanto <shojibul1991@gmail.com>
2024-09-15 11:42:38 +0600

    ds

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,19 +10,16 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
+let token = localStorage.getItem('token');
 
-// import Echo from 'laravel-echo';
+if (token) {
+    window.axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+    // axios.get('/api/check-expired').then((response)=>{
+    //     next();
+    // }).catch((error)=>{
+    //     next({name : 'Login'});
+    // })
+} else {
+    //  console.log('no token');
+}
 
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
